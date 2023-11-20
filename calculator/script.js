8b8c5aae82fdfb00f330ee0e135c9c6b2868c564
Yannawat Somon <yannawat.s@kkumail.com>
2023-11-20 22:17:11 +0700

    calculator v2 : change input 0 cond, input operator cond, error catching when doing calculation when press '=',  html some element class change to editer, add style for .editer

--- calculator/script.js
+++ calculator/script.js
@@ -2,19 +2,23 @@ const equal = document.getElementById("equal");
 const display = document.getElementById("result");
 const num0 = document.getElementById("0");
 const num00 = document.getElementById("00");
-
-function calculate() {}
+const operators = document.querySelectorAll(".operator");
 
 display.addEventListener("click", () => {
   console.log(display.value);
 });
 
 equal.addEventListener("click", () => {
-  display.value = eval(display.value);
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "";
+    alert("Wrong number operation");
+  }
 });
 
 num0.addEventListener("click", () => {
-  if (display.value !== "") {
+  if (display.value !== "0") {
     display.value += "0";
   }
 });
@@ -22,4 +26,12 @@ num00.addEventListener("click", () => {
   if (display.value !== "") {
     display.value += "00";
   }
+});
+
+operators.forEach(function (button) {
+  button.addEventListener("click", () => {
+    if (display.value !== "") {
+      display.value += button.value;
+    }
+  });
 });
